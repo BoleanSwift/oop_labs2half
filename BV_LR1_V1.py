83977eb7fcce966541081d7769400af4bdaa0514
BoleanSwift <brytong@ukr.net>
2020-02-25 20:00:48 +0300

    creating function for bubblesort (not responding)

---
+++ BV_LR1_V1.py
@@ -0,0 +1,13 @@
+import glob
+
+team_point = 10
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in n:
+        for in j:
+            if arr[j][1] < arr[1][j + 1]:
+                arr[j], arr[j + 1] = arr [j +1], arr[j]
+    return arr
+
+teams_point = bubbleSort(teams_points)
